d6de2c512b7454be3df27e45f05d05871024073d
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-06-17 13:10:01 -0500

    Update moviesController.js

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,6 +1,7 @@
 const Apifeatures = require('../Utilities/APIFeatures')
 const Movie = require('../Model/movieModel')
 const asyncErrorHandler = require('../Utilities/asyncErrorHandler')
+const customError = require('../Utilities/customError')
 
 exports.getHighestRated = (req, res, next) => {
   req.query.limit = '5'
@@ -9,7 +10,7 @@ exports.getHighestRated = (req, res, next) => {
   next()
 }
 
-exports.getAllMovies = asyncErrorHandler(async (req, res) => {
+exports.getAllMovies = asyncErrorHandler(async (req, res, next) => {
   const features = new Apifeatures(Movie.find(), req.query)
     .filter()
     .sort()
@@ -18,59 +19,76 @@ exports.getAllMovies = asyncErrorHandler(async (req, res) => {
 
   let movies = await features.query
 
-  const totalMovies = res.status(200).json({
+  res.status(200).json({
     status: 'Success',
     length: movies.length,
     data: {
-      movies,
+      movies: movies,
     },
   })
 })
 
-exports.createMovie = asyncErrorHandler(async (req, res) => {
+exports.createMovie = asyncErrorHandler(async (req, res, next) => {
   const movie = await Movie.create(req.body)
 
   res.status(200).json({
     status: 'Success',
-
     data: {
-      movie,
+      movie: movie,
     },
   })
 })
 
-exports.getMovieById = asyncErrorHandler(async (req, res) => {
+exports.getMovieById = asyncErrorHandler(async (req, res, next) => {
   const movieByID = await Movie.findById(req.params.id)
+
+  if (!movieByID) {
+    return next(new customError('Movie with that ID is not found!!', 404))
+  }
 
   res.status(200).json({
     status: 'Success',
-    data: movieByID,
+    data: {
+      movie: movieByID,
+    },
   })
 })
 
-exports.updateMovieById = asyncErrorHandler(async (req, res) => {
-  const updatemovie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
+exports.updateMovieById = asyncErrorHandler(async (req, res, next) => {
+  const updateMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   })
 
+  if (!updateMovie) {
+    return next(new customError('Movie with that ID is not found', 404))
+  }
+
   res.status(200).json({
     status: 'Success',
-    data: updatemovie,
+    data: {
+      movie: updateMovie,
+    },
   })
 })
 
-exports.deleteMovieById = asyncErrorHandler(async (req, res) => {
+exports.deleteMovieById = asyncErrorHandler(async (req, res, next) => {
   const deleteMovie = await Movie.findByIdAndDelete(req.params.id)
+
+  if (!deleteMovie) {
+    return next(new customError('Movie with that ID is not found', 404))
+  }
 
   res.status(200).json({
     status: 'Success',
     message: 'The movie is deleted',
-    data: deleteMovie,
+    data: {
+      movie: deleteMovie,
+    },
   })
 })
 
-exports.getMovieStats = asyncErrorHandler(async (req, res) => {
+exports.getMovieStats = asyncErrorHandler(async (req, res, next) => {
   const stats = await Movie.aggregate([
     { $match: { ratings: { $gte: 4.5 } } },
     {
@@ -96,7 +114,7 @@ exports.getMovieStats = asyncErrorHandler(async (req, res) => {
   })
 })
 
-exports.getMoviesByGenre = asyncErrorHandler(async (req, res) => {
+exports.getMoviesByGenre = asyncErrorHandler(async (req, res, next) => {
   const genreP = req.params.genre
 
   const moviesByGenre = await Movie.aggregate([
